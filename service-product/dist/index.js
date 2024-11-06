886690230c646f981fb4b249f6c98409360b3801
Juan Rey <93360398+rey-sudo@users.noreply.github.com>
2024-11-06 08:57:14 -0500

    6-11-2024

--- service-product/dist/index.js
+++ service-product/dist/index.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import http from 'http';
 import cors from 'cors';
+import { typeDefs } from './graphql/types';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
-
 const books = [
     {
         title: 'The Awakening',
@@ -15,72 +15,24 @@ const books = [
         author: 'Paul Auster',
     },
 ];
-
-
-const typeDefs = `#graphql
-
-type Product {
-  id: String
-  seller_id: String
-  name: String
-  model: String
-  features: String
-  terms_of_sale: String
-  guarantee: String
-  category: String
-  price: Int
-  collateral: Int
-  discount: Int
-  stock: Int
-  keywords: String
-  media_url: String
-  media_path: String
-  image_main: String
-  image_set: String
-  video_set: String
-}
-
-type Book {
-    title: String
-    autor: String
-}
-type Query {
-  getProduct(id: String): [Product]
-  books: [Book]
-}
-
-type Mutation {
-  createProduct(name: String!): Product!
-}
-
-`;
-
 const resolvers = {
     Query: {
         books: () => books,
     },
     Mutation: {
-        createProduct: (parent: any, args: any) => {
+        createProduct: (parent, args) => {
             const newProduct = {
                 title: args.name,
                 author: '2',
             };
-
             books.push(newProduct);
-
             console.log(books);
-
             return newProduct;
         }
     }
 };
-
-
-
 const app = express();
-
-const corsOrigin = process.env.CORS_DOMAINS!;
-
+const corsOrigin = process.env.CORS_DOMAINS;
 const corsOptions = {
     origin: corsOrigin?.split(",") || "*",
     methods: ["GET", "POST"],
@@ -90,34 +42,18 @@ const corsOptions = {
     exposedHeaders: ["Set-Cookie"],
     optionsSuccessStatus: 204,
 };
-
 const httpServer = http.createServer(app);
-
 const server = new ApolloServer({
     typeDefs,
     resolvers,
     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
 });
-
 const main = async () => {
     await server.start();
-
-    app.use(
-        "/api/product/graphql",
-        cors<cors.CorsRequest>(corsOptions),
-        express.json(),
-        expressMiddleware(server, {
-            context: async ({ req }) => ({ token: null }),
-        })
-    );
-
-    await new Promise<void>((resolve) =>
-        httpServer.listen({ port: 4000 }, resolve)
-    );
+    app.use("/api/product/graphql", cors(corsOptions), express.json(), expressMiddleware(server, {
+        context: async ({ req }) => ({ token: null }),
+    }));
+    await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
 };
-
 main();
-
-
-
-
+//# sourceMappingURL=index.js.map
