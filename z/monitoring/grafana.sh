946ce0daf6b6a50b8325201b5891fe84f5a38bf0
Juan Rey <93360398+rey-sudo@users.noreply.github.com>
2024-11-19 19:23:08 -0500

    19-11-2024

---
+++ z/monitoring/grafana.sh
@@ -0,0 +1,9 @@
+kubectl get secret --namespace default monitoring-grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo
+     
+     
+#export POD_NAME=$(kubectl get pods --namespace default -l "app.kubernetes.io/name=grafana,app.kubernetes.io/instance=monitoring" -o jsonpath="{.items[0].metadata.name}")
+#     kubectl --namespace default port-forward $POD_NAME 3000
+
+
+
+helm upgrade monitoring prometheus-community/kube-prometheus-stack -f values.yaml --set prometheus-node-exporter.hostRootFsMount.enabled=false
