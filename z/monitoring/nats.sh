4b78a1135026fc17a0cbb82414e1fe7a46da7861
Juan Rey <93360398+rey-sudo@users.noreply.github.com>
2024-11-21 21:40:49 -0500

    x

--- z/monitoring/nats.sh
+++ z/monitoring/nats.sh
@@ -1,2 +1,10 @@
-helm install nats-exporter prometheus-community/prometheus-nats-exporter
+
+helm uninstall nats-exporter prometheus-community/prometheus-nats-exporter
+
+helm install nats-exporter prometheus-community/prometheus-nats-exporter \
+  --set nats.url="http://nats:8222" \
+  --set service.port=7777 \
+  --set service.targetPort=7777 
+
+
 
