2849a16a1893c7ec84f97d8994ce3e2b3d21c257
Juan Rey <93360398+rey-sudo@users.noreply.github.com>
2024-11-22 20:41:56 -0500

    Update index.ts

--- base-consumer/src/utils/index.ts
+++ base-consumer/src/utils/index.ts
@@ -1,17 +1,26 @@
 import { Logger } from "tslog";
 import { customAlphabet } from "nanoid";
+import { database } from "../db/client.js";
 
-const logger = new Logger({ name: "service-product", 
-    prettyLogTemplate: "{{logLevelName}} {{dateIsoStr}} {{fileNameWithLine}}",
-    type: "pretty"
- });
+const logger = new Logger({
+  name: "service-product",
+  prettyLogTemplate: "{{logLevelName}} {{dateIsoStr}} {{fileNameWithLine}}",
+  type: "pretty",
+});
 
-const catcher = (message?: any, error?: any, bypass?: boolean) => {
-    logger.error(`EXIT=>${message}-${error}`);
+const catcher = async (message?: any, error?: any, bypass?: boolean) => {
+  try {
+    await database.client.end();
+    logger.info("DB CLOSED");
+  } catch (error: any) {
+    logger.error("DB_END_ERROR:", error);
+  }
 
-    return bypass || process.exit(1);
+  logger.error(`EXIT=>${message}-${error}`);
+
+  return bypass || process.exit(1);
 };
 
 const generateId = customAlphabet("0123456789ABCDEFGHIKLMNOPQRSTUVWXYZ", 15);
 
-export { logger, catcher, generateId }
+export { logger, catcher, generateId };
