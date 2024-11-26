b78907cb71475a7c71be1693ce2c9a6233c3c5bb
Juan Rey <93360398+rey-sudo@users.noreply.github.com>
2024-11-26 09:00:21 -0500

    26-11-2024 - style

--- frontend-seller/src/components/NavMenu.vue
+++ frontend-seller/src/components/NavMenu.vue
@@ -1,34 +1,30 @@
-<script setup>
-
-</script>
-
 <template>
   <div class="wrap">
     <img class="logo" src="@/assets/logo.svg" alt="logo">
     <nav>
       <ul>
-        <li>
+        <li :class="{ actived: currentRoute === 'home' }">
           <RouterLink to="/">
             <i class="pi pi-home" />
             <span>Home</span>
           </RouterLink>
         </li>
 
-        <li>
+        <li :class="{ actived: currentRoute === 'product-list' }">
           <RouterLink to="/product-list">
             <i class="pi pi-list" />
             <span>Product List</span>
           </RouterLink>
         </li>
 
-        <li>
+        <li :class="{ actived: currentRoute === 'create-product' }">
           <RouterLink to="/create-product">
             <i class="pi pi-plus" />
             <span>Create Product</span>
           </RouterLink>
         </li>
 
-        <li>
+        <li :class="{ actived: currentRoute === 'product-books' }">
           <RouterLink to="/product-books">
             <i class="pi pi-folder" />
             <span>Product Books</span>
@@ -42,12 +38,32 @@
   </div>
 </template>
 
+<script setup>
+import { ref, watch, onBeforeUnmount } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+const currentRoute = ref("/");
+
+const unwatchRoute = watch(
+  () => route.name,
+  (name) => currentRoute.value = name,
+  { immediate: true }
+);
+
+onBeforeUnmount(() => {
+  unwatchRoute()
+});
+</script>
+
+
 <style scoped>
 .wrap {
   background-color: var(--primary-a);
   margin-left: 0;
   width: 17rem;
-  padding: 1rem;
+
   z-index: 10;
   height: 100%;
   border-radius: 0 1.5rem 1.5rem 0;
@@ -72,9 +88,10 @@ li {
   align-items: center;
   position: relative;
   font-weight: 500;
-
   box-shadow: none;
   cursor: pointer;
+  font-size: var(--text-size-a);
+  border: 1px solid transparent;
 }
 
 li i {
@@ -88,18 +105,25 @@ li span {
 }
 
 a {
-  padding: 0.5rem 1rem;
+  padding: 0.5rem;
+  padding-left: 1rem;
+  padding-right: 1rem;
   display: flex;
   align-items: center;
 }
 
 .logo {
   margin-bottom: 1rem;
+  padding: 1rem;
 }
 
 .label {
   color: var(--text-w);
   font-size: var(--text-size-a);
   font-weight: 700;
+}
+
+.actived {
+  border-left: 2px solid white;
 }
 </style>
