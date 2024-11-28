2dccfca5780de86aaa7b116fca44f85697691dfd
Juan Rey <93360398+rey-sudo@users.noreply.github.com>
2024-11-27 20:35:17 -0500

    Update CreateProduct.vue

--- frontend-seller/src/views/CreateProduct.vue
+++ frontend-seller/src/views/CreateProduct.vue
@@ -59,100 +59,100 @@
                             </InputGroup>
                         </div>
                     </div>
-                    <div class="left-column-item">
-                        <div v-if="editor" class="editor" :class="{ invalid: formErrors.features }">
-                            <div class="editor-control">
-                                <div class="editor-control-group">
-                                    <button @click="editor.chain().focus().toggleBold().run()"
-                                        :disabled="!editor.can().chain().focus().toggleBold().run()"
-                                        :class="{ 'is-active': editor.isActive('bold') }">
 
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                            stroke-width="1.5" stroke="currentColor" class="size-6">
-                                            <path stroke-linejoin="round"
-                                                d="M6.75 3.744h-.753v8.25h7.125a4.125 4.125 0 0 0 0-8.25H6.75Zm0 0v.38m0 16.122h6.747a4.5 4.5 0 0 0 0-9.001h-7.5v9h.753Zm0 0v-.37m0-15.751h6a3.75 3.75 0 1 1 0 7.5h-6m0-7.5v7.5m0 0v8.25m0-8.25h6.375a4.125 4.125 0 0 1 0 8.25H6.75m.747-15.38h4.875a3.375 3.375 0 0 1 0 6.75H7.497v-6.75Zm0 7.5h5.25a3.75 3.75 0 0 1 0 7.5h-5.25v-7.5Z" />
-                                        </svg>
-                                    </button>
+                    <div v-if="editor" class="editor" :class="{ invalid: formErrors.features }">
+                        <div class="editor-control">
+                            <div class="editor-control-group">
+                                <button @click="editor.chain().focus().toggleBold().run()"
+                                    :disabled="!editor.can().chain().focus().toggleBold().run()"
+                                    :class="{ 'is-active': editor.isActive('bold') }">
 
-                                    <button @click="editor.chain().focus().toggleItalic().run()"
-                                        :disabled="!editor.can().chain().focus().toggleItalic().run()"
-                                        :class="{ 'is-active': editor.isActive('italic') }">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                            stroke-width="1.5" stroke="currentColor" class="size-6">
-                                            <path stroke-linecap="round" stroke-linejoin="round"
-                                                d="M5.248 20.246H9.05m0 0h3.696m-3.696 0 5.893-16.502m0 0h-3.697m3.697 0h3.803" />
-                                        </svg>
-                                    </button>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="size-6">
+                                        <path stroke-linejoin="round"
+                                            d="M6.75 3.744h-.753v8.25h7.125a4.125 4.125 0 0 0 0-8.25H6.75Zm0 0v.38m0 16.122h6.747a4.5 4.5 0 0 0 0-9.001h-7.5v9h.753Zm0 0v-.37m0-15.751h6a3.75 3.75 0 1 1 0 7.5h-6m0-7.5v7.5m0 0v8.25m0-8.25h6.375a4.125 4.125 0 0 1 0 8.25H6.75m.747-15.38h4.875a3.375 3.375 0 0 1 0 6.75H7.497v-6.75Zm0 7.5h5.25a3.75 3.75 0 0 1 0 7.5h-5.25v-7.5Z" />
+                                    </svg>
+                                </button>
 
-                                    <button @click="editor.chain().focus().toggleStrike().run()"
-                                        :disabled="!editor.can().chain().focus().toggleStrike().run()"
-                                        :class="{ 'is-active': editor.isActive('strike') }">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                            stroke-width="1.5" stroke="currentColor" class="size-6">
-                                            <path stroke-linecap="round" stroke-linejoin="round"
-                                                d="M12 12a8.912 8.912 0 0 1-.318-.079c-1.585-.424-2.904-1.247-3.76-2.236-.873-1.009-1.265-2.19-.968-3.301.59-2.2 3.663-3.29 6.863-2.432A8.186 8.186 0 0 1 16.5 5.21M6.42 17.81c.857.99 2.176 1.812 3.761 2.237 3.2.858 6.274-.23 6.863-2.431.233-.868.044-1.779-.465-2.617M3.75 12h16.5" />
-                                        </svg>
-                                    </button>
-                                </div>
+                                <button @click="editor.chain().focus().toggleItalic().run()"
+                                    :disabled="!editor.can().chain().focus().toggleItalic().run()"
+                                    :class="{ 'is-active': editor.isActive('italic') }">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="size-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M5.248 20.246H9.05m0 0h3.696m-3.696 0 5.893-16.502m0 0h-3.697m3.697 0h3.803" />
+                                    </svg>
+                                </button>
 
-                                <div class="editor-control-group">
-                                    <button @click="editor.chain().focus().toggleHeading({ level: 1 }).run()"
-                                        :class="{ 'is-active': editor.isActive('heading', { level: 1 }) }">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                            stroke-width="1.5" stroke="currentColor" class="size-6">
-                                            <path stroke-linecap="round" stroke-linejoin="round"
-                                                d="M2.243 4.493v7.5m0 0v7.502m0-7.501h10.5m0-7.5v7.5m0 0v7.501m4.501-8.627 2.25-1.5v10.126m0 0h-2.25m2.25 0h2.25" />
-                                        </svg>
-
-                                    </button>
-                                    <button @click="editor.chain().focus().toggleHeading({ level: 2 }).run()"
-                                        :class="{ 'is-active': editor.isActive('heading', { level: 2 }) }">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                            stroke-width="1.5" stroke="currentColor" class="size-6">
-                                            <path stroke-linecap="round" stroke-linejoin="round"
-                                                d="M21.75 19.5H16.5v-1.609a2.25 2.25 0 0 1 1.244-2.012l2.89-1.445c.651-.326 1.116-.955 1.116-1.683 0-.498-.04-.987-.118-1.463-.135-.825-.835-1.422-1.668-1.489a15.202 15.202 0 0 0-3.464.12M2.243 4.492v7.5m0 0v7.502m0-7.501h10.5m0-7.5v7.5m0 0v7.501" />
-                                        </svg>
-
-                                    </button>
-
-                                    <button @click="editor.chain().focus().toggleHeading({ level: 3 }).run()"
-                                        :class="{ 'is-active': editor.isActive('heading', { level: 3 }) }">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                            stroke-width="1.5" stroke="currentColor" class="size-6">
-                                            <path stroke-linecap="round" stroke-linejoin="round"
-                                                d="M20.905 14.626a4.52 4.52 0 0 1 .738 3.603c-.154.695-.794 1.143-1.504 1.208a15.194 15.194 0 0 1-3.639-.104m4.405-4.707a4.52 4.52 0 0 0 .738-3.603c-.154-.696-.794-1.144-1.504-1.209a15.19 15.19 0 0 0-3.639.104m4.405 4.708H18M2.243 4.493v7.5m0 0v7.502m0-7.501h10.5m0-7.5v7.5m0 0v7.501" />
-                                        </svg>
-                                    </button>
-                                </div>
-
-                                <div class="editor-control-group">
-                                    <button @click="editor.chain().focus().toggleBulletList().run()"
-                                        :class="{ 'is-active': editor.isActive('bulletList') }">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                            stroke-width="1.5" stroke="currentColor" class="size-6">
-                                            <path stroke-linecap="round" stroke-linejoin="round"
-                                                d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0ZM3.75 12h.007v.008H3.75V12Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm-.375 5.25h.007v.008H3.75v-.008Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
-                                        </svg>
-
-                                    </button>
-                                    <button @click="editor.chain().focus().toggleOrderedList().run()"
-                                        :class="{ 'is-active': editor.isActive('orderedList') }">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                            stroke-width="1.5" stroke="currentColor" class="size-6">
-                                            <path stroke-linecap="round" stroke-linejoin="round"
-                                                d="M8.242 5.992h12m-12 6.003H20.24m-12 5.999h12M4.117 7.495v-3.75H2.99m1.125 3.75H2.99m1.125 0H5.24m-1.92 2.577a1.125 1.125 0 1 1 1.591 1.59l-1.83 1.83h2.16M2.99 15.745h1.125a1.125 1.125 0 0 1 0 2.25H3.74m0-.002h.375a1.125 1.125 0 0 1 0 2.25H2.99" />
-                                        </svg>
-                                    </button>
-                                </div>
-
-                                <span class="editor-control-counter">
-                                    {{ productDiscountComputed }} / {{ editorLimit }}
-                                </span>
+                                <button @click="editor.chain().focus().toggleStrike().run()"
+                                    :disabled="!editor.can().chain().focus().toggleStrike().run()"
+                                    :class="{ 'is-active': editor.isActive('strike') }">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="size-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M12 12a8.912 8.912 0 0 1-.318-.079c-1.585-.424-2.904-1.247-3.76-2.236-.873-1.009-1.265-2.19-.968-3.301.59-2.2 3.663-3.29 6.863-2.432A8.186 8.186 0 0 1 16.5 5.21M6.42 17.81c.857.99 2.176 1.812 3.761 2.237 3.2.858 6.274-.23 6.863-2.431.233-.868.044-1.779-.465-2.617M3.75 12h16.5" />
+                                    </svg>
+                                </button>
                             </div>
 
-                            <editor-content :editor="editor" />
+                            <div class="editor-control-group">
+                                <button @click="editor.chain().focus().toggleHeading({ level: 1 }).run()"
+                                    :class="{ 'is-active': editor.isActive('heading', { level: 1 }) }">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="size-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M2.243 4.493v7.5m0 0v7.502m0-7.501h10.5m0-7.5v7.5m0 0v7.501m4.501-8.627 2.25-1.5v10.126m0 0h-2.25m2.25 0h2.25" />
+                                    </svg>
+
+                                </button>
+                                <button @click="editor.chain().focus().toggleHeading({ level: 2 }).run()"
+                                    :class="{ 'is-active': editor.isActive('heading', { level: 2 }) }">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="size-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M21.75 19.5H16.5v-1.609a2.25 2.25 0 0 1 1.244-2.012l2.89-1.445c.651-.326 1.116-.955 1.116-1.683 0-.498-.04-.987-.118-1.463-.135-.825-.835-1.422-1.668-1.489a15.202 15.202 0 0 0-3.464.12M2.243 4.492v7.5m0 0v7.502m0-7.501h10.5m0-7.5v7.5m0 0v7.501" />
+                                    </svg>
+
+                                </button>
+
+                                <button @click="editor.chain().focus().toggleHeading({ level: 3 }).run()"
+                                    :class="{ 'is-active': editor.isActive('heading', { level: 3 }) }">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="size-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M20.905 14.626a4.52 4.52 0 0 1 .738 3.603c-.154.695-.794 1.143-1.504 1.208a15.194 15.194 0 0 1-3.639-.104m4.405-4.707a4.52 4.52 0 0 0 .738-3.603c-.154-.696-.794-1.144-1.504-1.209a15.19 15.19 0 0 0-3.639.104m4.405 4.708H18M2.243 4.493v7.5m0 0v7.502m0-7.501h10.5m0-7.5v7.5m0 0v7.501" />
+                                    </svg>
+                                </button>
+                            </div>
+
+                            <div class="editor-control-group">
+                                <button @click="editor.chain().focus().toggleBulletList().run()"
+                                    :class="{ 'is-active': editor.isActive('bulletList') }">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="size-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0ZM3.75 12h.007v.008H3.75V12Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm-.375 5.25h.007v.008H3.75v-.008Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+                                    </svg>
+
+                                </button>
+                                <button @click="editor.chain().focus().toggleOrderedList().run()"
+                                    :class="{ 'is-active': editor.isActive('orderedList') }">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="size-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M8.242 5.992h12m-12 6.003H20.24m-12 5.999h12M4.117 7.495v-3.75H2.99m1.125 3.75H2.99m1.125 0H5.24m-1.92 2.577a1.125 1.125 0 1 1 1.591 1.59l-1.83 1.83h2.16M2.99 15.745h1.125a1.125 1.125 0 0 1 0 2.25H3.74m0-.002h.375a1.125 1.125 0 0 1 0 2.25H2.99" />
+                                    </svg>
+                                </button>
+                            </div>
+
+                            <span class="editor-control-counter">
+                                {{ productDiscountComputed }} / {{ editorLimit }}
+                            </span>
                         </div>
+
+                        <editor-content :editor="editor" />
                     </div>
+
                     <!--/////////////////////////////-->
                     <div class="left-column-item">
                         <div class="uploader" :class="{ invalid: formErrors.image_set }">
@@ -232,18 +232,19 @@
                 </div>
                 <div class="right-column">
                     <div class="box">
-                        <div class="subtitle">
+                        <div class="subtitle" v-tooltip="'Short product descriptions with great visibility'">
                             Bullet List
                         </div>
 
                         <div class="box-content">
-
                             <Button type="button" label="Use AI" :loading="sendMessageLoading" @click="beforeCreate"
                                 style="font-size: var(--text-size-a); margin-bottom: 1rem" variant="outlined"
                                 :disabled="!productDiscountComputed" />
 
-                            <Textarea v-model="productBulletList" rows="5" cols="30" autoResize fluid
-                                style="font-size: var(--text-size-a); min-height: 200px; max-height: 200px" />
+                            <AutoComplete inputId="productBulletList" v-model="productBulletList" multiple fluid
+                                :typeahead="false" :inputStyle="{ fontSize: 'var(--text-size-a)' }"
+                                :invalid="formErrors.keywords" size="large" placeholder="(↵)"
+                                removeTokenIcon="pi pi-minus" />
                         </div>
                     </div>
 
@@ -394,6 +395,9 @@ const productCollateral = ref(null);
 const productSKU = ref(null);
 const productModel = ref(null);
 const productBrand = ref(null);
+
+const productBulletList = ref(null);
+
 const productCategories = ref([
     { name: "Electronics", code: "electronics" },
     { name: "Books", code: "books" },
@@ -717,8 +721,7 @@ main {
     gap: 1rem;
 }
 
-.left-column-item {
-}
+.left-column-item {}
 
 .formulary {
     display: flex;
