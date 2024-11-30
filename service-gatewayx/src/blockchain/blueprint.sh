03e916086d57fac41bcbe5c9ede75499f4a62b47
Juan Rey <93360398+rey-sudo@users.noreply.github.com>
2024-11-30 09:34:36 -0500

    added contracts aiken validators folder

--- service-gatewayx/src/blockchain/blueprint.sh
+++ service-gatewayx/src/blockchain/blueprint.sh
@@ -1,3 +1,3 @@
 aiken build && npx @blaze-cardano/blueprint@latest plutus.json -o plutus.ts
-cp -f plutus.json ./test
-cp -f plutus.ts ./test
+#cp -f plutus.json ./builders
+cp -f plutus.ts ./builders
