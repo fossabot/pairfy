d96972ecae98f6837fbf990f9e8c67a1efc80a2d
Juan Rey <93360398+rey-sudo@users.noreply.github.com>
2024-12-12 18:35:45 -0500

    Update BuyModule.vue

--- frontend-main/src/views/product/BuyModule.vue
+++ frontend-main/src/views/product/BuyModule.vue
@@ -133,7 +133,7 @@ const quantityOptions = ref([
 const productRating = ref(4);
 
 const getStockLabel = (readyStock) => {
-    return readyStock > 0 ? "Product In Stock" : "Out Stock";
+    return readyStock > 0 ? "In Stock" : "Out Stock";
 }
 
 const showBuyDialog = ref(false);
