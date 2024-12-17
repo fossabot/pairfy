9597cef9fee87a226c57b46a4deec7bb58349da4
Juan Rey <93360398+rey-sudo@users.noreply.github.com>
2024-12-17 13:52:55 -0500

    delete gateway draft

--- service-gatewayx/src/blockchain/blueprint.sh
+++
@@ -1,3 +0,0 @@
-aiken build && npx @blaze-cardano/blueprint@latest plutus.json -o plutus.ts
-#cp -f plutus.json ./builders
-cp -f plutus.ts ./builders
--- service-gatewayx/src/utils/nano.ts
+++
@@ -1,5 +0,0 @@
-import { customAlphabet } from "nanoid";
-
-const getOrderId = customAlphabet("0123456789", 20);
-
-export { getOrderId };
