c35904e950698ed98b9ffcae31d1c6012ffb26fc
Juan Rey <93360398+rey-sudo@users.noreply.github.com>
2024-12-18 10:44:02 -0500

    Update HeaderComp.vue

--- frontend-main/src/components/header/HeaderComp.vue
+++ frontend-main/src/components/header/HeaderComp.vue
@@ -100,11 +100,11 @@ section {
     cursor: pointer;
     min-height: 20px;
     width: 79px;
-    background-image: url('../../assets/logo-white.png');  
+    background-image: url('../../assets/logo-white-p.png');  
     background-repeat: repeat-x;
     background-position: 0px 0px;
     width: 200px;
-    min-height: 60px;
+    min-height: 24px;
     background-repeat: no-repeat;
 }
 
