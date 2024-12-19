fd2d9db109e91faeec1668448292190705e87c7a
Juan Rey <93360398+rey-sudo@users.noreply.github.com>
2024-12-19 10:59:46 -0500

    added home

--- frontend-main/src/views/home/HomeView.vue
+++ frontend-main/src/views/home/HomeView.vue
@@ -1,9 +1,63 @@
+<template>
+  <main>
+    <div class="banner">
+      <div class="banner-body">
+        <div class="title">Next Black Day</div>
+        <div class="counter">
+          <span class="prefix">
+            0.000.
+          </span>
+          <div>100 ADA</div>
+        </div>
+      </div>
+    </div>
+    <div class="grid">
+
+    </div>
+  </main>
+</template>
+
 <script setup>
 
 </script>
 
-<template>
-  <main>
-  x
-  </main>
-</template>
+<style lang="css" scoped>
+main {
+  background: var(--black-a);
+  min-height: 100vh;
+}
+
+.banner {
+  height: 400px;
+  justify-content: center;
+  display: flex;
+  align-items: center;
+  color: var(--text-w);
+}
+
+.banner-body {
+  display: block;
+}
+
+.counter {
+  font-size: var(--text-size-5);
+  font-weight: 700;
+  display: flex;
+}
+
+.title {
+  font-size: var(--text-size-7);
+  font-weight: 700;
+}
+
+.prefix{
+  color: var(--text-b);
+}
+
+.grid {
+  border-top-left-radius: 24px;
+  border-top-right-radius: 24px;
+  height: 100vh;
+  background: var(--background-a);
+}
+</style>
