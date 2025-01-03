faa78e1a1c2601df1c18264bd4de1da16df08622
Juan Rey <93360398+rey-sudo@users.noreply.github.com>
2025-01-02 20:56:50 -0500

    Update NavComp.vue

--- frontend-main/src/components/header/NavComp.vue
+++ frontend-main/src/components/header/NavComp.vue
@@ -41,7 +41,7 @@ const { getADAprice } = headerAPI();
     margin: 0 2rem;
     font-size: var(--text-size-1);
     cursor: pointer;
-    font-weight: 500;
+    font-weight: 400; 
 }
 
 .categories {
