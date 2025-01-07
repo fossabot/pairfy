d7e0471f3a6648bd41575d72781ad75c939ed003
Juan Rey <93360398+rey-sudo@users.noreply.github.com>
2025-01-07 13:17:00 -0500

    Update AddressComp.vue

--- frontend-main/src/views/order/AddressComp.vue
+++ frontend-main/src/views/order/AddressComp.vue
@@ -1,10 +1,14 @@
 <template>
     <div class="card" v-if="addressData">
         <div class="card-head flex">
-           <span> Dispatch To</span>
+            <span> Dispatch To</span>
+            <i class="pi pi-truck"/>
         </div>
         <div class="card-body">
-            {{ addressData }}
+            <div class="card-item flex" v-for="(value, key) in addressData" :key="key">
+                <span>{{ key }}</span>
+                <span>{{ value }}</span>
+            </div>
         </div>
     </div>
 </template>
@@ -20,9 +24,11 @@ const addressData = computed(() => {
     let data = getOrderData.value?.address;
 
     if (data) {
-        const addressParsed = JSON.parse(Buffer.from(data, 'base64').toString("utf-8"));
+        const address64 = JSON.parse(Buffer.from(data, 'base64').toString("utf-8"));
 
-        return Buffer.from(addressParsed.data, 'base64').toString("utf-8")
+        const string = Buffer.from(address64.data, 'base64').toString("utf-8");
+
+        return JSON.parse(string)
     }
 
     return null
@@ -43,9 +49,28 @@ const addressData = computed(() => {
     border-bottom: 1px solid var(--border-a);
 }
 
-.card-body{
-    padding: 1rem;
+.card-head i:nth-child(2){
+    margin-left: 0.5rem;
 }
 
+.card-body {
+    padding: 1rem;
+    display: flex;
+    flex-direction: column;
+}
 
+.card-item {
+    justify-content: space-between;
+    text-transform: capitalize;
+    color: var(--text-b);
+    line-height: 2.5rem;
+    font-size: var(--text-size-1);
+    font-weight: 500;
+}
+
+.card-item span:nth-child(2) {
+    text-transform: capitalize;
+    max-width: 50%;
+    line-height: 2rem;
+}
 </style>
