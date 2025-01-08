55701fc24bde8f9735fcb03bbab786ac4d2c3883
Juan Rey <93360398+rey-sudo@users.noreply.github.com>
2025-01-08 08:53:13 -0500

    category icons no colors

--- frontend-main/src/views/home/CategoryComp.vue
+++ frontend-main/src/views/home/CategoryComp.vue
@@ -1,7 +1,13 @@
 <template>
     <div class="category">
         <div class="tag-body">
-            <div class="tag" v-for="(category, index) in categoryList" :key="index">{{ category.name }}</div>
+            <div class="tag" v-for="(category, index) in categoryList" :key="index">
+
+                <img :src="getURL(index)" alt="">
+
+                <span>{{ category.name }}</span>
+
+            </div>
         </div>
     </div>
 </template>
@@ -9,11 +15,49 @@
 <script setup>
 import categories from '@/assets/categories.json';
 import { ref } from 'vue';
+import asset0 from "@/assets/icons/0.svg";
+import asset1 from "@/assets/icons/1.svg";
+import asset2 from "@/assets/icons/2.svg"; 
+import asset3 from "@/assets/icons/3.svg";
+import asset4 from "@/assets/icons/4.svg";
+import asset5 from "@/assets/icons/5.svg";
+import asset6 from "@/assets/icons/6.svg";
+import asset7 from "@/assets/icons/7.svg";
+import asset8 from "@/assets/icons/8.svg";
+import asset9 from "@/assets/icons/9.svg";
+import asset10 from "@/assets/icons/10.svg";
+import asset11 from "@/assets/icons/11.svg";
+import asset12 from "@/assets/icons/12.svg"; 
+import asset13 from "@/assets/icons/13.svg"; 
 
-const categoryList = ref(categories)
+const categoryList = ref(categories);
+
+const assets = ref({
+    0: asset0, 
+    1: asset1,
+    2: asset2,
+    3: asset3,
+    4: asset4,
+    5: asset5,
+    6: asset6,
+    7: asset7,
+    8: asset8,
+    9: asset9,
+    10: asset10,
+    11: asset11,
+    12: asset12,
+    13: asset13
+});
+
+const getURL = (index) => {
+    return assets.value[index]
+}
 </script>
 
 <style lang="css" scoped>
+
+
+
 .category {
     width: 100%;
     display: flex;
@@ -41,10 +85,20 @@ const categoryList = ref(categories)
     white-space: nowrap;
     text-overflow: ellipsis;
     overflow: hidden;
-    display: inline-block;
+    display: flex;
+    align-items: center;
 }
 
 .tag:hover {
     background: var(--soft-red-a);
+}
+
+.tag img {
+    width: var(--text-size-3);
+    height: var(--text-size-3);
+}
+
+.tag span {
+    margin-left: 0.5rem;
 }
 </style>
