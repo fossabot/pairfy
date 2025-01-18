395154c5dd80c53f03868b54d1d5002dcaa00bd9
Juan Rey <93360398+rey-sudo@users.noreply.github.com>
2025-01-18 11:41:39 -0500

    Update AgentButton.vue

--- frontend-main/src/components/header/AgentButton.vue
+++ frontend-main/src/components/header/AgentButton.vue
@@ -39,13 +39,12 @@ const { togglePanel, getCurrentUser, setADAprice } = headerAPI();
     border-radius: 999px;
     display: flex;
     align-items: center;
-    color: inherit;
+    color: var(--text-b);
 }
 
 .card-address {
     font-size: var(--text-size-1);
     padding: 0 1rem;
-    color: var(--text-b);
 }
 
 .card-button {
