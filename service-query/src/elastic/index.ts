b1790a0d17b924e61b2065be0cc97d63c02fa1a4
Juan Rey <93360398+rey-sudo@users.noreply.github.com>
2025-02-01 18:50:26 -0500

    books

--- service-query/src/elastic/index.ts
+++ service-query/src/elastic/index.ts
@@ -57,11 +57,11 @@ const createProductIndex = async () => {
                 image: { type: "text", index: false }, // Store only
                 keywords: { type: "text", analyzer: "standard" }, // Full-text search
                 rating: { type: "float" }, // For sorting
-                reviews: { type: "integer" }, // For sorting
+                review_count: { type: "integer" }, // For sorting
                 discount: { type: "boolean" }, // For filtering
                 discount_value: { type: "double" }, // For sorting
                 best_seller: { type: "boolean" }, // For filtering
-                units_sold: { type: "integer" }  // For sorting
+                sold_count: { type: "integer" }  // For sorting
               },
             },
           },
