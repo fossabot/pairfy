479552c9aa78e922bafbe89d4d0bbf01b380f5c5
Juan Rey <93360398+rey-sudo@users.noreply.github.com>
2025-03-07 12:21:34 -0500

    Update NormalGrid.vue

--- frontend-main/src/views/home/NormalGrid.vue
+++ frontend-main/src/views/home/NormalGrid.vue
@@ -46,7 +46,7 @@ const props = defineProps(['content', 'title'])
 }
 
 .icon {
-  background: var(--pastel-6);
+  background: var(--background-b);
   justify-content: center;
   align-items: center;
   margin-left: 0.5rem;
