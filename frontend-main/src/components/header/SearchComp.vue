71e348c338323f616d242a4111a5fd4e72acd3ad
Juan Rey <93360398+rey-sudo@users.noreply.github.com>
2025-03-23 17:06:12 -0500

    Update SearchComp.vue

--- frontend-main/src/components/header/SearchComp.vue
+++ frontend-main/src/components/header/SearchComp.vue
@@ -30,7 +30,14 @@
                 </svg>
             </div>
             <div class="search-filter-button">
-                Price
+                <span> Order by</span>
+
+
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none"
+                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                    class="lucide lucide-chevron-down">
+                    <path d="m6 9 6 6 6-6" />
+                </svg>
             </div>
             <div class="search-filter-button">
                 Price
@@ -140,7 +147,7 @@ onBeforeUnmount(() => {
 
 .search-filter {
     justify-content: space-between;
-    padding: 0.5rem 0;
+    padding: 0.75rem 0;
     display: none;
 }
 
