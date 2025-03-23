48956724cd8b9137cf924a0e7c27c9fe2f78c4c3
Juan Rey <93360398+rey-sudo@users.noreply.github.com>
2025-03-22 20:16:26 -0500

    Update LocationComp.vue

--- frontend-main/src/components/header/LocationComp.vue
+++ frontend-main/src/components/header/LocationComp.vue
@@ -69,9 +69,16 @@
             </span>
             <span class="separator" />
             <div class="icon flex">
-                <i class="pi pi-map-marker" />
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
+                    stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"
+                    class="lucide lucide-map-pin">
+                    <path
+                        d="M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0" />
+                    <circle cx="12" cy="10" r="3" />
+                </svg>
+                <span>{{ getLocationData?.name }}</span>
             </div>
-            <span>{{ getLocationData?.name }}</span>
+
         </div>
     </div>
 </template>
@@ -174,12 +181,12 @@ onBeforeUnmount(() => {
 }
 
 .icon {
-    height: inherit;
     margin-right: 0.5rem;
+    height: inherit;
 }
 
-.icon i {
-    font-size: var(--text-size-2);
+.icon span {
+    margin-left: 0.5rem;
 }
 
 .box {
@@ -231,7 +238,7 @@ onBeforeUnmount(() => {
 .separator {
     background: var(--text-b);
     margin: 0 1rem;
-    height: 1rem; 
+    height: 1rem;
     width: 1px;
 }
 
