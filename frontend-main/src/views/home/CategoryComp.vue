b1f8c96d9298433b7b84c61a1d8eea2b21850a61
Juan Rey <93360398+rey-sudo@users.noreply.github.com>
2025-04-03 13:42:23 -0500

    updates

--- frontend-main/src/views/home/CategoryComp.vue
+++ frontend-main/src/views/home/CategoryComp.vue
@@ -21,6 +21,7 @@ import image1 from '@/assets/icons/1.png'
 import image4 from '@/assets/icons/4.png'
 import image7 from '@/assets/icons/7.png'
 import image8 from '@/assets/icons/8.png'
+import image10 from '@/assets/icons/10.png'
 import image13 from '@/assets/icons/13.png'
 import image14 from '@/assets/icons/14.png'
 
@@ -34,6 +35,7 @@ categories.value[1].icon = image1
 categories.value[4].icon = image4
 categories.value[7].icon = image7
 categories.value[8].icon = image8
+categories.value[10].icon = image10 
 
 categories.value[13].icon = image13
 categories.value[14].icon = image14
