4b09bf413d110b45857cfa9883f483af8784e336
Juan Rey <93360398+rey-sudo@users.noreply.github.com>
2025-04-03 19:25:52 -0500

    Update RatingComp.vue

--- frontend-main/src/components/RatingComp.vue
+++ frontend-main/src/components/RatingComp.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex space-x-1">
-    <svg v-for="index in 5" :key="index" width="14px" height="14px" viewBox="0 0 24 24" 
+    <svg v-for="index in 5" :key="index" width="1rem" height="1rem" viewBox="0 0 24 24" 
       xmlns="http://www.w3.org/2000/svg"
       :fill="index <= rating ? 'var(--orange-a)' : 'none'"
       :stroke="index <= rating ? 'none' : 'var(--border-a)'"
