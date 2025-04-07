b1b6613f1a58a15aa0ae5f9706ac472ae3f75709
Juan Rey <93360398+rey-sudo@users.noreply.github.com>
2025-04-07 11:41:19 -0500

    updated

---
+++ frontend-seller/src/components/HeaderComp.vue
@@ -0,0 +1,21 @@
+<template>
+      <div class="header flex">
+        <img class="logo" src="@/assets/logo.svg" alt="">
+      </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="css" scoped>
+.header {
+  border-bottom: 1px solid var(--border-a);
+  background: var(--background-a);
+  padding: 0.25rem 1rem;
+}
+
+.logo {
+  height: 2.5rem;
+}
+</style>
