a802f77d7bee9155a189b8a8e5afcc57f4e8f843
Juan Rey <93360398+rey-sudo@users.noreply.github.com>
2025-04-08 17:30:21 -0500

    updated

--- frontend-seller/nuxt.config.ts
+++ frontend-seller/nuxt.config.ts
@@ -1,11 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   runtimeConfig: {
-    serviceSellerBase: 'http://service-seller.default.svc.cluster.local:8000/api',
-    serviceProductBase: 'http://service-product.default.svc.cluster.local:8000/api',
-
+    serviceSellerBase: process.env.NUXT_SERVICE_SELLER_BASE,
+    serviceProductBase: process.env.NUXT_SERVICE_PRODUCT_BASE,
     public: {
       apiBaseBrowser: ''
     }
--- frontend-seller/stores/auth.ts
+++ frontend-seller/stores/auth.ts
@@ -1,63 +1,70 @@
-import { defineStore } from 'pinia'
+import { defineStore } from "pinia";
 
-export const useAuthStore = defineStore('auth', () => {
-  const isAuthenticated = useState<boolean>('isAuthenticated', () => false)
-  const seller = useState<any>('seller', () => null)
-  const loading = ref(false)
-  const error = ref<string | null>(null)
+export const useAuthStore = defineStore("auth", () => {
+  const isAuthenticated = useState<boolean>("isAuthenticated", () => false);
+  const seller = useState<any>("seller", () => null);
+  const loading = ref(false);
+  const error = ref<string | null>(null);
 
-  const config = useRuntimeConfig()
+  const config = useRuntimeConfig();
+
+  console.log('💡 NUXT_SERVICE_SELLER_BASE:', process.env.NUXT_SERVICE_SELLER_BASE)
 
   const login = async (credentials: { email: string; password: string }) => {
-    loading.value = true
-    error.value = null
+    loading.value = true;
+    error.value = null;
 
     try {
       await $fetch(`${config.serviceSellerBase}/seller/login-seller`, {
-        method: 'POST',
+        method: "POST",
         body: credentials,
-        credentials: 'include' // aún necesario si backend devuelve cookie de sesión
-      })
+        credentials: "include", // aún necesario si backend devuelve cookie de sesión
+      });
 
-      await fetchProfile()
-      isAuthenticated.value = true
+      await fetchProfile();
+      isAuthenticated.value = true;
     } catch (err: any) {
-      error.value = err.data?.message || 'Login failed'
-      isAuthenticated.value = false
+      error.value = err.data?.message || "Login failed";
+      isAuthenticated.value = false;
     } finally {
-      loading.value = false
+      loading.value = false;
     }
-  }
+  };
 
   const fetchProfile = async () => {
+    if (!import.meta.server) return;
 
     try {
-      const data = await $fetch(`${config.serviceSellerBase}/seller/current-seller`, {
-        method: 'GET',
-        credentials: 'include'
-      })
+      const data = await $fetch(
+        `${config.serviceSellerBase}/seller/current-seller`,
+        {
+          method: "GET",
+          credentials: "include",
+        }
+      );
 
-      seller.value = data
-      isAuthenticated.value = true
-    } catch {
-      isAuthenticated.value = false
-      seller.value = null
+      seller.value = data;
+      isAuthenticated.value = true;
+    } catch (err: any) {
+      console.log(err);
+      isAuthenticated.value = false;
+      seller.value = null;
     }
-  }
+  };
 
   const logout = async () => {
     try {
       await $fetch(`${config.serviceSellerBase}/seller/logout-seller`, {
-        method: 'GET',
-        credentials: 'include'
-      })
+        method: "GET",
+        credentials: "include",
+      });
     } catch {
       // si falla, igual se limpia
     }
 
-    isAuthenticated.value = false
-    seller.value = null
-  }
+    isAuthenticated.value = false;
+    seller.value = null;
+  };
 
   return {
     isAuthenticated,
@@ -66,6 +73,6 @@ export const useAuthStore = defineStore('auth', () => {
     error,
     login,
     logout,
-    fetchProfile
-  }
-})
+    fetchProfile,
+  };
+});
