c7301769d1dc0d9af728a2c66ac7f6448d7e0882
Juan Rey <93360398+rey-sudo@users.noreply.github.com>
2025-04-09 16:59:13 -0500

    updated

--- common/src/executeSql.ts
+++
@@ -1,7 +0,0 @@
-import fs from 'fs';
-import path from 'path';
-
-export function loadSqlFile(filename: string): string {
-  const fullPath = path.join(__dirname, '../sql', filename);
-  return fs.readFileSync(fullPath, 'utf-8');
-}
