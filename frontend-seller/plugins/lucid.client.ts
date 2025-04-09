dab0d476077ffaab7c4faa4b1966c01622090861
Juan Rey <93360398+rey-sudo@users.noreply.github.com>
2025-04-09 07:21:13 -0500

    Delete lucid.client.ts

--- frontend-seller/plugins/lucid.client.ts
+++
@@ -1,7 +0,0 @@
-import { Lucid } from 'lucid-cardano'
-
-export default defineNuxtPlugin(async (nuxtApp) => {
-  if (!import.meta.client) return // importante para evitar SSR
-
-  nuxtApp.provide('lucid', Lucid)
-})
