75b70733e28ca6a27f2e70d926309bc1c299b2ff
Juan Rey <93360398+rey-sudo@users.noreply.github.com>
2025-04-11 13:42:08 -0500

    ofetch experimental working

--- frontend-seller/server/api/seller/verify-seller.post.ts
+++ frontend-seller/server/api/seller/verify-seller.post.ts
@@ -1,4 +1,5 @@
 import { ofetch } from "ofetch";
+import { throwRemoteError } from "~/server/utils/fetch";
 
 export default defineEventHandler(async (event) => {
   const config = useRuntimeConfig();
@@ -12,7 +13,6 @@ export default defineEventHandler(async (event) => {
         method: "POST",
         body,
         async onResponseError({ response }) {
-          console.log("TEST1", response._data);
           throw new Error(JSON.stringify(response._data));
         },
       }
@@ -20,21 +20,6 @@ export default defineEventHandler(async (event) => {
 
     return response;
   } catch (err: any) {
-    console.log("TEST1.2", err);
-    // Esta lógica intenta rescatar el error real del backend
-    const originalError =
-      err?.data || // ofetch: backend body (JSON)
-      err?.response?._data || // fallback: Nuxt internal fetch structure
-      err?.response?.statusMessage || // fallback: textual status
-      err?.message || // FetchError default
-      "Unknown server error"; // ultra fallback
-
-    console.error("🔥 Error capturado del backend:", originalError);
-
-    throw createError({
-      statusCode: err?.response?.status || err?.statusCode || 500,
-      statusMessage: "Remote Service Error",
-      data: originalError,
-    });
+    throwRemoteError(err);
   }
 });
--- frontend-seller/stores/auth.ts
+++ frontend-seller/stores/auth.ts
@@ -86,12 +86,10 @@ export const useAuthStore = defineStore("auth", () => {
         method: "POST",
         body: credentials,
         async onResponseError({ response }) {
-          console.log("TEST2", response._data.data);
           throw new Error(JSON.stringify(response._data.data));
         }
       });
     } catch (err: any) {
-      console.log("TEST3", JSON.stringify(err))
       throw new Error(err.message);
     } finally {
       loading.value = false;
