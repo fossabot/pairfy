483d75495bed76190dc73be3d96fc080e67fdaa6
Juan Rey <93360398+rey-sudo@users.noreply.github.com>
2025-04-16 17:23:24 -0500

    Update default.vue

--- frontend-seller/layouts/default.vue
+++ frontend-seller/layouts/default.vue
@@ -2,27 +2,65 @@
   <div class="layout">
     <aside :class="['sidebar', { collapsed: isCollapsed && !isHovering }]" @mouseenter="isHovering = true"
       @mouseleave="isHovering = false">
+
+      <div class="layout-icon">
+        <img src="@/assets/brand/icon.svg" alt="">
+      </div>
       <nav>
         <NuxtLink to="/">
           <span class="icon">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-house-icon lucide-house"><path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"/><path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+              stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"
+              class="lucide lucide-house-icon lucide-house">
+              <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8" />
+              <path
+                d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+            </svg>
           </span>
           <span class="label" :class="{ collapsed: isCollapsed && !isHovering }"
             v-show="!isCollapsed || isHovering">Home</span>
         </NuxtLink>
         <NuxtLink to="/dashboard">
           <span class="icon">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-plus-icon lucide-circle-plus"><circle cx="12" cy="12" r="10"/><path d="M8 12h8"/><path d="M12 8v8"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+              stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"
+              class="lucide lucide-circle-plus-icon lucide-circle-plus">
+              <circle cx="12" cy="12" r="10" />
+              <path d="M8 12h8" />
+              <path d="M12 8v8" />
+            </svg>
           </span>
           <span class="label" :class="{ collapsed: isCollapsed && !isHovering }"
             v-show="!isCollapsed || isHovering">Create Product</span>
         </NuxtLink>
         <NuxtLink to="/settings">
           <span class="icon">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-square-chart-gantt-icon lucide-square-chart-gantt"><rect width="18" height="18" x="3" y="3" rx="2"/><path d="M9 8h7"/><path d="M8 12h6"/><path d="M11 16h5"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+              stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"
+              class="lucide lucide-square-chart-gantt-icon lucide-square-chart-gantt">
+              <rect width="18" height="18" x="3" y="3" rx="2" />
+              <path d="M9 8h7" />
+              <path d="M8 12h6" />
+              <path d="M11 16h5" />
+            </svg>
           </span>
           <span class="label" :class="{ collapsed: isCollapsed && !isHovering }"
             v-show="!isCollapsed || isHovering">Product List</span>
+        </NuxtLink>
+
+        <NuxtLink to="/settings">
+          <span class="icon">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+              stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"
+              class="lucide lucide-square-chart-gantt-icon lucide-square-chart-gantt">
+              <rect width="18" height="18" x="3" y="3" rx="2" />
+              <path d="M9 8h7" />
+              <path d="M8 12h6" />
+              <path d="M11 16h5" />
+            </svg>
+          </span>
+          <span class="label" :class="{ collapsed: isCollapsed && !isHovering }"
+            v-show="!isCollapsed || isHovering">Product Books</span>
         </NuxtLink>
       </nav>
     </aside>
@@ -62,8 +100,8 @@ if (import.meta.server) {
 
 
 nav {
-  margin-top: 50px;
   flex-direction: column;
+  margin-top: 1rem;
   display: flex;
   gap: 0rem;
 }
@@ -100,4 +138,14 @@ nav a:hover {
   visibility: hidden;
 }
 
+.layout-icon {
+  justify-content: center;
+  display: flex;
+  height: 64px;
+  width: 64px;
+}
+
+.layout-icon img {
+  width: 80%;
+}
 </style>
