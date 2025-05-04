ab7f410940d3b140d9f8e81a547d90889c5db516
Juan Rey <93360398+rey-sudo@users.noreply.github.com>
2025-05-03 23:26:30 -0500

    removed e.g placeholder

--- frontend-seller/components/InputProductSku.vue
+++ frontend-seller/components/InputProductSku.vue
@@ -28,7 +28,7 @@ const props = defineProps({
   id: { type: String, default: 'sku' },
   modelValue: { type: String, default: '' },
   label: { type: String, default: 'SKU' },
-  placeholder: { type: String, default: 'e.g. TV55-SAMSUNG-2025' },
+  placeholder: { type: String, default: 'TV55-SAMSUNG-2025' },
   focus: { type: Boolean, default: false },
   required: { type: Boolean, default: true },
   maxLength: { type: Number, default: 20 },
