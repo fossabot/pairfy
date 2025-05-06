a1d7a9a8f0917dd39a6df94642d9376e2d1ae190
Juan Rey <93360398+rey-sudo@users.noreply.github.com>
2025-05-05 23:43:25 -0500

    Update product-list.vue

--- frontend-seller/pages/product-list.vue
+++ frontend-seller/pages/product-list.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="card">
-        <FolderComp :tabs="['Products', 'Statistics']">
+        <FolderComp :tabs="['Products', 'Statistics']" v-model="tabIndex">
 
             <template #icon-0>
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
