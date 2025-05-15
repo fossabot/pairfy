50b8df49d3ac9d07febee1dd9339b058b69e3dd1
Juan Rey <93360398+rey-sudo@users.noreply.github.com>
2025-05-14 19:06:24 -0500

    Update edit-product.ts

--- service-product/src/graphql/handlers/edit-product.ts
+++ service-product/src/graphql/handlers/edit-product.ts
@@ -11,9 +11,10 @@ import {
   updateProduct,
 } from "@pairfy/common";
 import { verifyParams } from "../../validators/edit-product.js";
+import { checkFileGroup } from "../../utils/media.js";
+import { applyDiscount } from "../../utils/index.js";
 
 export const editProduct = async (_: any, args: any, context: any) => {
-
   let connection = null;
 
   try {
@@ -34,7 +35,7 @@ export const editProduct = async (_: any, args: any, context: any) => {
       args.editProductInput.description
     );
 
-    ////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////
 
     connection = await database.client.getConnection();
 
@@ -43,6 +44,8 @@ export const editProduct = async (_: any, args: any, context: any) => {
     const SELLER = context.sellerData as SellerToken;
 
     const timestamp = Date.now();
+
+    /////////////////////////////////////////////////////////////////////////////////////
 
     const findProduct = await findProductBySeller(
       connection,
@@ -56,11 +59,36 @@ export const editProduct = async (_: any, args: any, context: any) => {
       });
     }
 
+    if(findProduct.media_group_id !== params.media_group_id){
+      throw new ApiGraphQLError(400, "Invalid MediaGroupId", {
+        code: ERROR_CODES.VALIDATION_ERROR,
+      });
+    }
+
     await connection.beginTransaction();
 
-    /////////////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////////// START TRANSACTION
+    
+    const isValidGroup = await checkFileGroup(
+      "http://service-media.default.svc.cluster.local:8003/api/media/verify-group",
+      {
+        agent_id: SELLER.id,
+        media_group_id: params.media_group_id,
+        file_ids: params.file_ids,
+        product_id: params.id
+      },
+      process.env.INTERNAL_ENDPOINT_SECRET as string
+    );
+
+    if (!isValidGroup) {
+      throw new ApiGraphQLError(409, "Inconsistency of image ids.", {
+        code: ERROR_CODES.CONFLICT,
+      });
+    }
 
     const updateScheme: any = {
+      media_position: params.file_ids,
+      thumbnail_url: isValidGroup.thumbnail_url,
       name: params.name,
       price: params.price,
       sku: params.sku,
@@ -75,7 +103,9 @@ export const editProduct = async (_: any, args: any, context: any) => {
       city: params.city,
       postal: params.postal,
       discount: params.discount,
+      discount_value: applyDiscount(params.price, params.discount_percent),
       discount_percent: params.discount_percent,
+      updated_at: timestamp,
       schema_v: findProduct.schema_v + 1,
     };
 
@@ -89,9 +119,13 @@ export const editProduct = async (_: any, args: any, context: any) => {
     );
 
     if (update.affectedRows !== 1) {
-      throw new ApiGraphQLError(409, "Update failed: version mismatch or not found", {
-        code: ERROR_CODES.UPDATE_CONFLICT,
-      });
+      throw new ApiGraphQLError(
+        409,
+        "Update failed: version mismatch or not found",
+        {
+          code: ERROR_CODES.UPDATE_CONFLICT,
+        }
+      );
     }
 
     const updatedProduct = await findProductById(connection, findProduct.id);
@@ -105,7 +139,7 @@ export const editProduct = async (_: any, args: any, context: any) => {
       SELLER.id
     );
 
-    /////////////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////////// END TRANSACTION
 
     await connection.commit();
 
