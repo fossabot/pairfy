a17f6fa973f4ba99848f4a051cc8d6cdfac6f144
Juan Rey <93360398+rey-sudo@users.noreply.github.com>
2025-05-17 15:28:48 -0500

    Update BannerComp.vue

--- frontend-main/components/BannerComp.vue
+++ frontend-main/components/BannerComp.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="banner-wrapper">
-        <section class="banner">
+    <div class="Banner">
+        <div class="Banner-body">
             <div class="overlay">
 
                 <!-- Header -->
-                <div class="banner-header">
-                    <div class="header-left">
+                <div class="Banner-header">
+                    <div class="Banner-header-left">
                         <h1>
                             <span class="bold">Cardano</span>
                             <span class="light">native marketplace</span>
@@ -18,7 +18,7 @@
                         </ul>
                     </div>
 
-                    <div class="header-right">
+                    <div class="Banner-header-right">
                         <p><strong>5M+</strong> Factory direct supply</p>
                         <p><strong>10</strong> Local warehouses worldwide</p>
                         <p><strong>20M+</strong> Value dropshipping items</p>
@@ -26,24 +26,24 @@
                     </div>
                 </div>
 
-                <!-- Product Sections -->
-                <div class="product-sections">
-                    <div class="category">
+                <!-- Cards Sections -->
+                <div class="Banner-cards">
+                    <div class="box">
                         <h2>Discount pool</h2>
                         <div class="cards">1.000.000 ADA / 53%</div>
                     </div>
-                    <div class="category">
+                    <div class="box">
                         <h2>Discount airdrop</h2>
                         <div class="cards">43 days left</div>
                     </div>
-                    <div class="category">
+                    <div class="box">
                         <h2>Benefited users</h2>
                         <div class="cards">2000 wallets</div>
                     </div>
                 </div>
 
             </div>
-        </section>
+        </div>
     </div>
 </template>
 
@@ -52,31 +52,28 @@
 </script>
 
 <style scoped>
-.banner-wrapper {
+.Banner {
     justify-content: center;
     display: flex;
     width: 100%;
 }
 
-.banner {
+.Banner-body {
     width: 100%;
-    padding: 1rem;
     position: relative;
-    border-radius: 1rem;
     box-sizing: border-box;
     max-width: var(--body-a);
     backdrop-filter: blur(10px);
-    background: rgba(0, 0, 0, 0.1);
+    border-radius: var(--radius-c);
+    background: rgba(0, 0, 0, 0.4);
 }
 
 .overlay {
-    background: rgba(0, 0, 0, 0.4);
     color: var(--text-w);
-    border-radius: 1rem;
     padding: 2rem;
 }
 
-.banner-header {
+.Banner-header {
     gap: 2rem;
     display: flex;
     flex-wrap: wrap;
@@ -86,9 +83,8 @@
 
 }
 
-.header-left h1 {
+.Banner-header-left h1 {
     font-size: var(--text-size-8);
-    /* 48px */
     margin-bottom: 1rem;
     margin: 0;
 }
@@ -115,40 +111,36 @@
     margin-bottom: 0.75rem;
 }
 
-.header-right {
+.Banner-header-right {
     font-size: var(--text-size-3);
-    /* 18px */
 }
 
-.header-right strong {
+.Banner-header-right strong {
     font-size: var(--text-size-4);
-    /* 20px */
 }
 
-.product-sections {
-    display: flex;
+.Banner-cards {
     gap: 2rem;
+    display: flex;
     flex-wrap: wrap;
 }
 
-.category {
+.box {
     flex: 1;
     min-width: 200px;
 }
 
-.category h2 {
+.box h2 {
     font-size: var(--text-size-5);
-    /* 24px */
-    margin-bottom: 0.5rem;
     margin-bottom: 0.75rem;
+    margin-bottom: 0.5rem;
 }
 
 .cards {
-    background: var(--card-bg, #ffffff20);
     padding: 1rem;
-    border-radius: 0.5rem;
+    backdrop-filter: blur(5px);
     font-size: var(--text-size-4);
-    /* 20px */
-    backdrop-filter: blur(4px);
+    border-radius: var(--radius-c);  
+    background: rgba(0, 0, 0, 0.1);
 }
 </style>
