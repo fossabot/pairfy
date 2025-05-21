8a51526913223bce49c382b304ba85e484d411d6
Juan Rey <93360398+rey-sudo@users.noreply.github.com>
2025-05-20 20:51:20 -0500

    Update ProductModel.vue

--- frontend-main/components/ProductModel.vue
+++ frontend-main/components/ProductModel.vue
@@ -1,19 +1,20 @@
 <template>
-    <div class="ProductButton flex">
-        <div class="icon">
+    <div class="ProductModel flex">
+        <div class="icon flex">
             <img src="https://m.media-amazon.com/images/I/61cCf94xIEL.__AC_SX300_SY300_QL70_FMwebp_.jpg" alt="">
         </div>
 
         <div class="body">
-            <div class="model">
+            <div class="model flex">
                 <span>Model S9-C52025</span>
+                <span class="divider" />
                 <span class="color" />
             </div>
 
             <div class="price">
                 <span>$38 USD</span>
-                <span class="discount">-10%</span>
-                <span class="saved">$250 saved</span>
+                <span class="discount">-10% OFF</span>
+                <span class="saved">Save $250</span>
             </div>
         </div>
     </div>
@@ -24,7 +25,7 @@
 </script>
 
 <style scoped>
-.ProductButton {
+.ProductModel {
     border: 1px solid rgba(0, 0, 0, 50%);
     transition: var(--transition-a);
     border-radius: var(--radius-c);
@@ -33,49 +34,36 @@
     padding: 1rem;
 }
 
-.ProductButton:focus {
+.ProductModel:focus {
     outline: 1px solid var(--primary-a);
 }
 
-.ProductButton:hover {
+.ProductModel:hover {
     border: 1px solid var(--primary-a);
 }
-
 
 .body {
     display: flex;
     flex-direction: column;
 }
 
-.icon {
-    width: 2rem;
-    height: 2rem;
-    object-fit: contain;
-}
-
-.model {
-    display: flex;
-    align-items: center;
-}
-
 .color {
-    background: black;
-    width: 1rem;
-    height: 1rem;
     border-radius: 50%;
-    margin-left: 0.5rem;
+    background: gray;
+    height: 1rem;
+    width: 1rem;
 }
 
 .price {
-    font-weight: 400;
     margin-top: 0.5rem;
+    font-weight: 400;
 }
 
 .icon {
     width: 2rem;
     height: 2rem;
-    object-fit: contain;
     margin-right: 1rem;
+    object-fit: contain;
 }
 
 .icon img {
@@ -93,9 +81,17 @@
 
 .discount {
     color: var(--red-a);
+    font-weight: 600;
 }
 
 .saved {
-    color: var(--green-a);
+    color: var(--text-b);
+}
+
+.divider {
+    width: 1px;
+    height: 0.75rem;
+    margin: 0 0.75rem;
+    background: var(--text-b)
 }
 </style>
